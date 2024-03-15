5ba067f81755cebd3621d88701cfb1b72b654148
Edvard-G <edvard.gelov@hotmail.com>
2024-03-15 19:00:50 +0100

    improved db.ts

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -3,17 +3,30 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
+const isDev: boolean = process.env.NODE_ENV === "development";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    port: parseInt(process.env.DB_PORT || "5432"),
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: "postgres",
+  port: parseInt(process.env.DB_PORT || "5432"),
+  logging: !isDev, 
+});
+
+const connectdb = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    if (isDev) {
+      await sequelize.sync({ alter: true });
+      console.log("Database synchronized with alter option.");
+    }
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+    process.exit(1);
   }
-);
+};
 
-export default sequelize
-
+export { sequelize, connectdb };
