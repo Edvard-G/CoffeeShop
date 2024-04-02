b98db2441497d06f853398fa851eb6ea15e63da9
Edvard-G <edvard.gelov@hotmail.com>
2024-04-02 17:51:28 +0200

    Implemented case-insensitive email login in Sequelize authentication

--- backend/util/helper.util.ts
+++ backend/util/helper.util.ts
@@ -1,18 +1,27 @@
 import bcrypt from "bcrypt";
 import User from "../models/userModel";
+import { sequelize } from "../config/db";
 
-export const hashPassword = async (password: string): Promise<string> => {
+const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 };
 
-export const authenticateUser = async (
+const authenticateUser = async (
   email: string,
   password: string
 ): Promise<User | null> => {
-  const user = await User.findOne({ where: { email } });
-  if (user && (await bcrypt.compare(password, user.password))) {
-    return user;
+  const user = await User.findOne({
+    where: sequelize.where(
+      sequelize.fn("LOWER", sequelize.col("email")),
+      sequelize.fn("LOWER", email)
+    ),
+  });
+
+  if(user && (await bcrypt.compare(password, user.password))) {
+    return user
   }
   return null;
 };
+
+export {hashPassword, authenticateUser};
