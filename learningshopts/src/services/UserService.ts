fdad0082cca50d60372a2ee09c784f2a7f90fd74
Edvard-G <edvard.gelov@hotmail.com>
2024-04-03 20:46:25 +0200

    Type mistake fixed

--- learningshopts/src/services/UserService.ts
+++ learningshopts/src/services/UserService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LoginFormSchema } from "../validations/ValidationSchema";
+import { LoginFormSchemaType } from "../validations/ValidationSchema";
 
 const server_url: string = import.meta.env.VITE_API_URL!;
 
@@ -7,7 +7,7 @@ if (!server_url) {
   throw Object.assign(new Error("Missing API URL"), { code: 500 });
 }
 
-const login = (data: LoginFormSchema) => {
+const login = (data: LoginFormSchemaType) => {
   return axios.post(`${server_url}/api/users/login`, data);
 };
 
